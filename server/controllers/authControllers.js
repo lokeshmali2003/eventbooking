700e5ac16a8afa285d4951958eaf920f6d19c8f1
Lokesh Mali <malilokesh311@gmail.com>
2026-04-27 23:18:34 +0530

    Email OTP Setup

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,8 @@
 const User = require('../models/User');
 const { sendOtpEmail } = require('../utils/email');
+const OTP = require('../models/OTP');
+const {sendOtpEmail} = require('../utils/email')
+
 
 //register User
 
@@ -13,8 +16,6 @@ exports.registerUser = async (req, res)=>{
 
      const salt = await bcrypt.genSalt(10);
      const hashedPassword = await bcrypt.hash(Password, salt);
-
-
 
     try{
         const user = new user.create ({name, email, Password: hashedPassword , role: 'user' , isVerified: false  });
@@ -35,3 +36,36 @@ exports.registerUser = async (req, res)=>{
         res.status(400).json({error: error.message});
     }
 };
+
+//Login User
+
+exports.loginUser = async(req,res)=>{
+    const  {email, password} = req.body;
+     
+    let user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({error:'Invalid Credentials, Please Signup '});
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({error:'Invalid Credentials'});
+    }
+
+    if(!user.isVerified){
+        const otp = Math.floor(100000 + Math.random()*900000).toString();
+        await OTP.deleteMany({email, action: 'account_verification'}); // Remove Old OTPs 
+        await OTP.create({email, otp, action: 'account_verification'});
+        await sendOtpEmail(email, otp, 'account_verification');
+        return res.status(400).json({
+            error:'Account Not Verified. A new Otp has been Sent to Your Email.'
+        });
+    }
+
+    res.status(200).json({
+        message: 'Login Successfully',
+        user:{
+
+        }
+    })
+}
