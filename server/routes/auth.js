5054cd6108b26c74d4bea6776032ad8e5971faad
Lokesh Mali <malilokesh311@gmail.com>
2026-05-01 08:25:13 +0530

    Setup Backend

--- server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {registerUser , loginUser , verifyOtp}= require('../controllers/authControllers');
-
-router.post('/register', registerUser);
-router.post('/login', loginUserUser);
-router.post('/verify-otp', verifyOtp);
